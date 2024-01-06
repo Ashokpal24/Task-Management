eb79d776b09e7445d57e7813e03771426f91dbe9
Ashokpal24 <88579810+Ashokpal24@users.noreply.github.com>
2024-01-06 13:31:45 +0000

    Added ProjectGroup.

--- server/Task_management/ProjectGroup/models.py
+++ server/Task_management/ProjectGroup/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from User.models import User
 
-# Create your models here.
+
+class ProjectGroup(models.Model):
+    title = models.CharField(
+        verbose_name="Title",
+        max_length=255,
+        blank=False)
+
+    description = models.CharField(
+        verbose_name="Description",
+        max_length=255,
+        blank=False)
+
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        verbose_name="User ID",
+        null=True)
+
+    created_at = models.DateTimeField(
+        verbose_name="Created At",
+        auto_now_add=True)
+
+    updated_at = models.DateTimeField(
+        verbose_name="Updated At",
+        auto_now=True)
+
+    def __str__(self):
+        return f"Title: {self.title}\n  Created by:{self.created_by}"
--- server/Task_management/Task_management/settings.py
+++ server/Task_management/Task_management/settings.py
@@ -43,8 +43,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'User',
+    'ProjectGroup',
     'Task',
-    'Subtask'
+    'Subtask',
 ]
 
 MIDDLEWARE = [
